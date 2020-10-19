82ec6208638bde8d125859deab5ee99d5454f696
Emre Elagoz <emreelagoz1994@gmail.com>
2020-10-19 23:34:00 +0200

    Add project files.

---
+++ MethodOefeningTwee/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodOefeningTwee
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+            GetMax(firstNumber, secondNumber, thirdNumber);
+
+        }
+
+        static void GetMax(int parameterOne, int parameterTwo, int parameterThree)
+        {
+
+            if (parameterOne > parameterTwo && parameterOne > parameterThree)
+            {
+                Console.WriteLine($"The bigger number is: {parameterOne}");
+            }
+
+            if (parameterTwo > parameterOne && parameterTwo > parameterThree)
+            {
+                Console.WriteLine($"The bigger number is: {parameterTwo}");
+            }
+
+            if (parameterThree > parameterTwo && parameterThree > parameterOne)
+            {
+                Console.WriteLine($"The bigger number is: {parameterThree}");
+            }
+
+        }
+    }
+}
---
+++ MethodsOefenen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsOefenen
+{
+    class Program
+    {
+        static void Main()
+        {
+            AddName(Console.ReadLine());
+            
+        }
+
+        static void AddName(string userName)
+        {
+            Console.WriteLine($"Hallo, {userName}");
+        }
+    }
+}
